5a4f9e668b58dc98c7d41214e05d06943a34089a
Victor Matheus <imatheusfsantos@gmail.com>
2018-10-19 17:20:35 -0300

    initial commit

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+
 import numpy as np
 import csv
 
 reader = csv.reader(open('subdata.csv','r'), delimiter=',')
 
 rows = np.array(list(reader))
-labels = rows[0]
+labels = rows[0, 1:-1]
 print(labels)
 X = rows[2:-1, 1:-1]
 Y = rows[2:-1, -1]
 #print(rows)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 
 
-#print(X_train)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y)
+
+
+
+print(X_train)
+########## TESTE ################################
+
+model = Sequential()
+# Dense(64) is a fully-connected layer with 64 hidden units.
+# in the first layer, you must specify the expected input data shape:
+# here, 20-dimensional vectors.
+model.add(Dense(64, activation='relu', input_dim=20))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+model.fit(X_train, Y_train,
+          epochs=20,
+          batch_size=128)
+score = model.evaluate(X_test, Y_test, batch_size=128)
+
 #train_test_split()
 #print(labels)
