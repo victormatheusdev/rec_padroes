0edc9ba1d02c77a1fdb69d1d3cb57896550fbcf1
Your Name <you@example.com>
2018-10-22 17:22:18 -0300

    svm add

--- main3.py
+++ main3.py
@@ -21,18 +21,17 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, stratif
 
 model = Sequential()
 qnt_entradas = len(labels)
-model.add(Dense(30, input_dim=qnt_entradas, init='uniform', activation='relu'))
+model.add(Dense(128, input_dim=qnt_entradas, init='uniform', activation='relu'))
 
-model.add(Dense(30, init='uniform', activation='relu'))
-
-
+model.add(Dense(128, init='uniform', activation='relu'))
+model.add(Dense(128, init='uniform', activation='relu'))
 
 model.add(Dense(1, activation='sigmoid'))
 
 
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) #adam = descida do gradiente
-model.fit(X_train, Y_train, nb_epoch=50, batch_size=192)
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy']) #adam = descida do gradiente
+model.fit(X_train, Y_train, nb_epoch=50, batch_size=10)
 scores = model.evaluate(X_test, Y_test)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 print(qnt_entradas)
--- svm.py
+++ svm.py
@@ -1,3 +1,4 @@
+from sklearn import svm
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from keras.layers import Dense, Dropout, Activation
@@ -5,9 +6,11 @@ from keras.optimizers import SGD
 
 import numpy as np
 import csv
+from sklearn.metrics import classification_report, confusion_matrix
 np.random.seed(7) #fixar a semente pra sempre obter os mesmos resultados
 #N sei se funciona perfeitamente com a train_test_split
 from tensorflow.python.client import device_lib
+from sklearn.svm import SVC
 reader = csv.reader(open('data.csv','r'), delimiter=',')
 
 rows = np.array(list(reader))
@@ -15,26 +18,16 @@ labels = rows[1, 1:-1]
 X = rows[2:-1, 1:-1]
 Y = rows[2:-1, -1]
 #print(rows)
+print("WOOOOO2")
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, stratify=Y)
-########## TESTE ################################
 
-model = Sequential()
-qnt_entradas = len(labels)
-model.add(Dense(30, input_dim=qnt_entradas, init='uniform', activation='relu'))
+svclassifier = SVC(kernel='linear', verbose=True)
+print("WOOOOO2")
 
-model.add(Dense(30, init='uniform', activation='relu'))
+svclassifier.fit(X_train, Y_train)
+print("WOOOOO")
+y_pred = svclassifier.predict(X_test)
 
-
-
-model.add(Dense(1, activation='sigmoid'))
-
-
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) #adam = descida do gradiente
-model.fit(X_train, Y_train, nb_epoch=50, batch_size=192)
-scores = model.evaluate(X_test, Y_test)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-print(qnt_entradas)
-#train_test_split()
-#print(labels)
+print(confusion_matrix(Y_test,y_pred))
+print(classification_report(Y_test,y_pred))
